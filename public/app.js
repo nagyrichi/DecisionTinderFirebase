80c4a5817ae4c4148a9d6410708b4661b419cf21
nagyrichi <30602658+nagyrichi@users.noreply.github.com>
2025-07-21 10:15:46 +0200

    module

--- public/app.js
+++ public/app.js
@@ -1,3 +1,19 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDp3HumzdoZ6SWWQX6wPLmk0RzYl0qPbjs",
+  authDomain: "decisiontinderfirebase.firebaseapp.com",
+  projectId: "decisiontinderfirebase",
+  storageBucket: "decisiontinderfirebase.appspot.com",
+  messagingSenderId: "743649100420",
+  appId: "1:743649100420:web:18f276d16d855241dbf8d1",
+  measurementId: "G-2T7C1GQFXF"
+};
+
+// Itt NEM KELL delete() — a module miatt csak egyszer fut le
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+console.log("🔥 Firestore init done:", db);
+
 // --- Globális változók ---
 let topics = {}, currentTopic = null, currentItems = [], currentIndex = 0, votes = {}, decidedItems = new Set();
 let userId = null, sessionId = "global";
